8c35ad82f56d949a9c7fbef0e52dde9aecd44fd6
Toha Islam <islamtoha710@gmail.com>
2024-01-21 21:52:28 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="main">
+        <div className="loading">
+          <div className="glass"></div>
+          <div className="box2"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
